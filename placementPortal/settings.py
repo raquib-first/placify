68b48dd4dc81667c6ea96f90d46c07cd6a51513e
raquib <raquibmca24@cs.du.ac.in>
2026-05-31 01:02:54 +0530

    Clean requirements for Render deployment

--- placementPortal/settings.py
+++ placementPortal/settings.py
@@ -140,7 +140,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static'),
 ]
 
-STATIC_ROOT = os.path/join(BASE_DIR , "staticfiles")STORAGES = {
+STATIC_ROOT = os.path/join(BASE_DIR , "staticfiles")
+
+STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
